75cffff5db6e42af983799eb002e234b6ea04c4f
Natissa-DidyToure <natissadidy@gmail.com>
2023-10-07 17:52:49 -0400

    creation de lappli + ajout dans la composante principale les 3 variables

---
+++ mon-premier-exercice/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titrePrincipal = 'Mon Application Angular';
+  sousTitre = 'Bienvenue sur mon application';
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae er';
+}
